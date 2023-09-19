74e50dfa432f33debbb40b58c0bf1e0588849862
Bruno Da Costa Cunha <Bruno.DA_COSTA_CUNHA@etu.uca.fr>
2023-09-19 22:56:20 +0200

    Drone swager + tuto swager fait

--- Sources/OpenLibraryWrapper/Program.cs
+++ Sources/OpenLibraryWrapper/Program.cs
@@ -14,7 +14,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
