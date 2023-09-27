926abf887d9b85931d9b547812ff9ad31a50bd9d
étudiant <etudiant@MB-R-110-001-178.local>
2023-09-27 09:05:04 +0200

    Variable d'env

--- Sources/OpenLibraryWrapper/Program.cs
+++ Sources/OpenLibraryWrapper/Program.cs
@@ -9,6 +9,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+envar = Environment.GetEnvironmentVariable("DTO_MANAGER");
+
+if(envar == null || envar == "Stub" )
+{
+    builder.Services.AddSingleton<IDtoManager,Stub>();
+}
+else
+{
+    builder.Services.AddSingleton<IDtoManager, OpenLibClientAPI>();
+}
 builder.Services.AddSingleton<IDtoManager,OpenLibClientAPI>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
