caf5f6437908e4704f72b8091b930ab55c997f26
Bruno_MAC <brunocunha1506@gmail.com>
2023-10-04 08:36:36 +0200

    ajout de mylibrary

--- Sources/OpenLibraryWrapper/Program.cs
+++ Sources/OpenLibraryWrapper/Program.cs
@@ -2,18 +2,18 @@
 using DtoAbstractLayer;
 using LibraryDTO;
 using Microsoft.OpenApi.Models;
+using MyLibraryManager;
 using OpenLibraryClient;
 using StubbedDTO;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 var envar = Environment.GetEnvironmentVariable("DTO_MANAGER");
 
-if(envar == null || envar == "stub" )
+if (envar == null || envar == "stub")
 {
-    builder.Services.AddSingleton<IDtoManager,Stub>();
+    builder.Services.AddSingleton<IDtoManager, Stub>();
 }
 else if (envar == "api")
 {
@@ -24,7 +24,9 @@ else if (envar == "mariadb")
     builder.Services.AddSingleton<IDtoManager, MyLibraryMgr>();
 }
 
-builder.Services.AddSingleton<IDtoManager,OpenLibClientAPI>();
+// Add services to the container.
+
+builder.Services.AddSingleton<IDtoManager, Stub>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -46,4 +48,3 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
-
